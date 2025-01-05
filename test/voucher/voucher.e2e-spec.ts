dec640bc04e221e50ee86d6402f2fce7a41e3c27
Aibek Sadraliev <a.sadraliev@gmail.com>
2025-01-05 17:40:46 -0500

    test(voucher): add e2e test for voucher creation with fixed mode (#20)
    * chore(app): make command to clear redundant local branches
    
    * test(voucher): issue voucher with fixed mode

--- test/voucher/voucher.e2e-spec.ts
+++ test/voucher/voucher.e2e-spec.ts
@@ -50,28 +50,41 @@ describe('Voucher controller (e2e)', () => {
 
   afterAll(async () => {
     await connection.dropDatabase();
-    app.close();
+    await app.close();
     await connection.close();
   });
 
   it('Create a new voucher issued automatically.', async () => {
-    const createVoucherDto = createVoucher();
+    const withAuthMode = createVoucher((voucher) => ({
+      ...voucher,
+      policy: {
+        ...voucher.policy,
+        issue_mode: 'auto',
+      },
+    }));
     const userIdFromJwtToken = 'Jonh-Wick';
 
     const response = await request(app.getHttpServer())
       .post('/vouchers')
       .set('Authorization', `Bearer ${userIdFromJwtToken}`)
-      .send(createVoucherDto)
+      .send(withAuthMode)
       .expect(201);
 
-    expect(response.body).toEqual({ ok: true });
+    expect(response.body).toEqual({
+      ok: true,
+      payload: {
+        id: expect.any(String),
+      },
+    });
 
-    const voucher = await voucherModel.findOne().lean();
+    const voucher = await voucherModel
+      .findById(response.body.payload.id)
+      .lean();
     const policy = await policyModel.findById(voucher.policyId).lean();
     const reward = await rewardModel.findById(voucher.rewardId).lean();
 
     expect({
-      ...createVoucherDto,
+      ...withAuthMode,
       issued_by: userIdFromJwtToken,
     }).toEqual(
       expect.objectContaining({
@@ -88,6 +101,61 @@ describe('Voucher controller (e2e)', () => {
         policy: expect.objectContaining({
           name: policy.name,
           issue_mode: policy.issueMode,
+          stamps_required_for_reward: policy.stampsRequiredForReward,
+        }),
+      }),
+    );
+  });
+
+  it('Create a new voucher with fixed mode', async () => {
+    const withFixedMode = createVoucher((voucher) => ({
+      ...voucher,
+      policy: {
+        ...voucher.policy,
+        issue_mode: 'fixed',
+        max_reissue: 10,
+      },
+    }));
+    const userIdFromJwtToken = 'Jonh-Wick';
+
+    const response = await request(app.getHttpServer())
+      .post('/vouchers')
+      .set('Authorization', `Bearer ${userIdFromJwtToken}`)
+      .send(withFixedMode)
+      .expect(201);
+
+    expect(response.body).toEqual({
+      ok: true,
+      payload: {
+        id: expect.any(String),
+      },
+    });
+
+    const voucher = await voucherModel
+      .findById(response.body.payload.id)
+      .lean();
+    const policy = await policyModel.findById(voucher.policyId).lean();
+    const reward = await rewardModel.findById(voucher.rewardId).lean();
+
+    expect({
+      ...withFixedMode,
+      issued_by: userIdFromJwtToken,
+    }).toEqual(
+      expect.objectContaining({
+        name: voucher.name,
+        promotion_name: voucher.promotionName,
+        issued_by: voucher.issuedBy,
+        issue_at: new Date(voucher.issueAt).toISOString(),
+        expiration_at: new Date(voucher.expirationAt).toISOString(),
+        reward: expect.objectContaining({
+          name: reward.name,
+          description: reward.description,
+          terms_url: reward.termsUrl,
+        }),
+        policy: expect.objectContaining({
+          name: policy.name,
+          issue_mode: policy.issueMode,
+          max_reissue: policy.maxReissue,
           stamps_required_for_reward: policy.stampsRequiredForReward,
         }),
       }),
